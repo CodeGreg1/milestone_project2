ac399d83776509a07968cde53b1abfb9f5f97ed5
Greg Goodrem <code@greggoodrem.co.uk>
2022-07-24 22:35:00 +0100

    Simplifying the addevents and organising script.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ const GAME_SCORE = document.getElementById('gameScore')
 const NUMS = document.querySelectorAll('.nums span')
 const COUNTER = document.querySelector('.counter')
 const REPLAY = document.querySelector('#replay')
+const TARGETS = document.querySelectorAll('.target')
 const TARGET_1 = document.getElementById('targetone')
 const TARGET_2 = document.getElementById('targettwo')
 const TARGET_3 = document.getElementById('targetthree')
@@ -21,22 +22,10 @@ const TARGET_TALLY_ID = document.querySelector('#targetTally')
 const BULLET_HOLES = document.querySelectorAll('.ripple')
 const INSTRUCTION_START = document.getElementById('instructionStart')
 
-var positionNum = 5
+let positionNum = 5
 let perfectTally = 0
 let shotTally = 0
 let targetTally = 0
-
-PRESTART.addEventListener('click',runGame)
-
-TARGET_1.addEventListener('click',()=> {
-    positionNum = 268})
-TARGET_2.addEventListener('click',()=> {
-    positionNum = 288
-    incrementScore()})
-TARGET_3.addEventListener('click',()=> {
-    positionNum = 308
-    incrementScore()
-    incrementScore()})
 
 var sfx = {
     shot: new Howl({
@@ -46,57 +35,89 @@ var sfx = {
     miss: new Howl({
     src: ["https://codegreg1.github.io/milestone_project2/ricochet.mp3"],
     autoplay:true
+})}
+
+PRESTART.addEventListener('click',runGame)
+
+REPLAY.addEventListener('click', replay)
+
+INSTRUCTION_START.addEventListener('click', instructionStart)
+
+MISSED.addEventListener('click', missedShot)
+
+TARGET_1.addEventListener('click',()=> {
+    positionNum = 268
+    shotUpdate(1)})
+TARGET_2.addEventListener('click',()=> {
+    positionNum = 288
+    incrementScore()
+    shotUpdate(2)})
+TARGET_3.addEventListener('click',()=> {
+    positionNum = 308
+    shotUpdate(3)
+    incrementScore()
+    incrementScore()
 })
-}
-
-
-
-//These are my target event listeners for each row they will all create a gun sound, increment the total score and reset the position of the target that has dropped
-const TARGETS = document.querySelectorAll('.target')
 
 TARGETS.forEach((target)=>{
-    target.addEventListener('click',()=> {
+    target.addEventListener('click', function(e) {
         target.classList.toggle('active')
         sfx.shot.play()
         incrementScore()
         targetTally++
+        e.stopPropagation()
+        console.log('Target clicked')
     	setTimeout(newPosition, 100)})
-    })
+})
 
-// Perfect score increase
 CENTERS.forEach((center)=>{
     center.addEventListener('click',()=> {
         incrementScore()
         perfectTally++
         incrementPerfectScore()})
-    })
-
-MISSED.addEventListener('click', ()=> {
-         sfx.miss.play()
-         sfx.miss.volume(0.2)
-     
 })
 
-// Function creates a new position for the targets dependant on which one is chosen dictates the distance from the left border it will be using the positionNum variable
+BULLET_HOLES.forEach(hole => {
+    hole.addEventListener('click', function (e) {
+    const TARGET_OFFSET = parseInt(4)
+    const X = e.clientX - TARGET_OFFSET
+    const Y = e.clientY - TARGET_OFFSET
+    const circle = document.createElement('span')
+    circle.classList.add('circle')
+    circle.style.top = Y + 'px'
+    circle.style.left = X + 'px'
+    this.appendChild(circle)
+    console.log(X,Y)
+    shotUpdate()
+    setTimeout(() => circle.remove(), 200)
+})})
+    
+function missedShot(){
+    sfx.miss.play()
+    sfx.miss.volume(0.6)
+}
+
 function newPosition(){
     let activeTarget = document.querySelector('.target.active')
     activeTarget.style.left = `${(Math.floor(Math.random() * positionNum) + 1)}px`;
-    activeTarget.classList.toggle('active')}
+    activeTarget.classList.toggle('active')
+}
 
-// Function increments the score which is linked with the event listeners for the clicking of targets.
 function incrementPerfectScore(){
     let oldScore = parseInt(document.getElementById("perfectScore").innerText);
-    document.getElementById("perfectScore").innerText = ++oldScore;}
+    document.getElementById("perfectScore").innerText = ++oldScore;
+}
 
-// Function for Perfect score tally throughout the current game 
 function incrementScore(){
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;}
+    let oldScore = parseInt(document.getElementById("score").innerText)
+    document.getElementById("score").innerText = ++oldScore
+    
+}
 
-// Function runs the game by initiating the count down sequence 
 function runGame() {
     perfectTally = 0
     shotTally = 0
+    SHOT_TALLY_CURRENT.innerText = shotTally
     targetTally = 0
     resetDOM()
     startScore()
@@ -107,9 +128,8 @@ function runGame() {
     NUMBERS.classList.add('in')
     NUMBERS.classList.remove('in')
     NUMBERS.classList.add('out')
-    }
+}
 
-//This function puts the end score in the h2 identifying a top score however I believe it runs beyond required so need to understand this bug
 function stopScore () {
     FINAL_SCORE.innerText = `${parseInt(FINAL_SCORE.innerText)} Top Score!`
     SCORE_SCREEN.classList.remove('hiddenscore')
@@ -122,7 +142,6 @@ function stopScore () {
     allScores()
 }
 
-// Function will reset the DOM so that the game can be replayed 
 function resetDOM () {
     COUNTER.classList.remove('hide')
     COUNTER.classList.add('show')
@@ -131,7 +150,6 @@ function resetDOM () {
     NUMS[0].classList.add('in')
 }
 
-// Function runs the animations for the count down from 20 to zero.
 function runAnimation() {
     NUMS.forEach((num,idx) => {
         let nextToLast = NUMS.length - 1
@@ -142,9 +160,10 @@ function runAnimation() {
             } else if (e.animationName === 'goOut' && num.nextElementSibling) {
                 num.nextElementSibling.classList.add('in')
             } else {
-                COUNTER.classList.add('hide')}})})}
+                COUNTER.classList.add('hide')}})})
+}
 
-REPLAY.addEventListener('click', () => {
+function replay(){
     FINAL_SCORE.innerText = '0'
     PERFECT_SCORE.innerText = '0'
     SHOT_TALLY_CURRENT.innerText = '0'
@@ -152,11 +171,10 @@ REPLAY.addEventListener('click', () => {
     SCORE_SCREEN.classList.remove('visible')
     PERFECT_SCORE.classList.remove('hiddenscore')
     PERFECT.classList.remove('hiddenscore')
-    runGame()  
-})
+    runGame() 
+}
 
-// The Event listener is operated from the score screen 
-INSTRUCTION_START.addEventListener('click', () => {
+function instructionStart(){
     FINAL_SCORE.innerText = '0'
     PERFECT_SCORE.innerText = '0'
     SCORE_SCREEN.classList.add('hiddenscore')
@@ -165,37 +183,24 @@ INSTRUCTION_START.addEventListener('click', () => {
     PERFECT.classList.remove('hiddenscore')
     PRESTART.classList.remove('hiddenscore')
     PRESTART.classList.add('visible')
-})
+}
 
-// Function will start the timer for the game to then stop the game after the setTimeOut
 function startScore (){
     PRESTART.classList.add('hiddenscore')
     PRESTART.classList.remove('visible')
     setTimeout(stopScore, 20000)
 }
-BULLET_HOLES.forEach(hole => {
-    hole.addEventListener('click', function (e) {
-    const TARGET_OFFSET = parseInt(4)
-    const X = e.clientX - TARGET_OFFSET
-    const Y = e.clientY - TARGET_OFFSET
-    const circle = document.createElement('span')
-    circle.classList.add('circle')
-    circle.style.top = Y + 'px'
-    circle.style.left = X + 'px'
-    this.appendChild(circle)
-    console.log(X,Y)
+
+function shotUpdate(){
     shotTally++
     SHOT_TALLY_CURRENT.innerText = shotTally
-    setTimeout(() => circle.remove(), 200)
-})})
+}
 
 function allScores (){ 
     SHOT_TALLY_ID.innerHTML = `${shotTally} Shots Taken`
     if(shotTally === 0){
-        shotTally++
-    }
+        shotTally++}
     let perfectPercentage = Math.floor(perfectTally/shotTally*100)
     TARGET_TALLY_ID.innerHTML = `${targetTally} Targets Hit`
     PERFECT_TALLY_ID.innerHTML = `${perfectPercentage}% Perfect Shots`
-    console.log(typeof(perfectPercentage))
 }
