708052f3ff59558b53ef330de9c573ee6d4be395
Greg Goodrem <code@greggoodrem.co.uk>
2022-07-29 10:30:03 +0100

    Anitated the JS document and reorganised it.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
+//Elements related to the basic visual setup and interaction
 const NUMBERS = document.getElementById('numbers')
 const FINAL_SCORE = document.getElementById('score')
 const PERFECT_SCORE = document.getElementById('perfectScore')
 const SCORE_SCREEN = document.getElementById('scorescreen')
 const PRESTART = document.getElementById('prestart')
 const GAME_SCORE = document.getElementById('gameScore')
-//More specific to the countdown timer variables
 const NUMS = document.querySelectorAll('.nums span')
 const COUNTER = document.querySelector('.counter')
 const REPLAY = document.querySelector('#replay')
@@ -21,7 +21,7 @@ const PERFECT_TALLY_ID = document.querySelector('#perfectTally')
 const TARGET_TALLY_ID = document.querySelector('#targetTally')
 const BULLET_HOLES = document.querySelectorAll('.ripple')
 const INSTRUCTION_START = document.getElementById('instructionStart')
-
+//Active variables throughout game
 let positionNum = 5
 let perfectTally = 0
 let shotTally = 0
@@ -73,6 +73,12 @@ TARGET_3.addEventListener('click',()=> {
     incrementScore(3)
 }
 )
+/* These for loops are for when targets get tapped/clicked and a 
+firing sound is to be initiated. The stopPropagation makes sure it
+doesn't start the missed shot event listener. 
+The second is a for loop for the detection of a perfect shot being made.
+The third is the visualisation of a shot being made on the container.*/
+
 TARGETS.forEach((target)=>{
     target.addEventListener('click', function(e) {
         target.classList.toggle('active')
@@ -122,6 +128,9 @@ function newPosition(){
     activeTarget.classList.toggle('active')
 }
 
+/* Below are the two different incrementation of scores. The perfect score is very basic
+but the general incrementScore incorporates numbers being passed into it from whichever 
+level the targets are hit. */
 function incrementPerfectScore(){
     let oldScore = parseInt(document.getElementById("perfectScore").innerText);
     document.getElementById("perfectScore").innerText = ++oldScore;
@@ -136,6 +145,15 @@ function incrementScore(x){
     document.getElementById("score").innerText = oldScore + x
 }
 
+/* Below are the main game starting and restarting functions. 
+runGame() is the initial function ready to be started at the start of the page load 
+and runs other functions for the game to be active.
+resetDOM() is used to reset the DOM and also the countdown timer.
+replay() is used after the game has been run so that the scores can go back to zero 
+and also the DOM being reset upon the next game load.
+instructionStart() is an alternative option to replay() incase anyone playing needs to 
+review the instructions again however things still need to be reset.
+*/
 function runGame() {
     perfectTally = 0
     shotTally = 0
@@ -152,37 +170,12 @@ function runGame() {
     NUMBERS.classList.add('out')
 }
 
-function stopScore () {
-    FINAL_SCORE.innerText = `${parseInt(FINAL_SCORE.innerText)} Top Score!`
-    SCORE_SCREEN.classList.remove('hiddenscore')
-    SCORE_SCREEN.classList.add('visible')
-    PERFECT_SCORE.classList.add('hiddenscore')
-    PERFECT.classList.add('hiddenscore')
-    GAME_SCORE.innerText = FINAL_SCORE.innerText
-    FINAL_SCORE.style.fontSize = '35px'
-    NUMBERS.classList.remove('out')
-    allScores()
-}
-
 function resetDOM () {
     COUNTER.classList.remove('hide')
     COUNTER.classList.add('show')
     NUMS.forEach((num) => {
         num.classList.value = ''})
     NUMS[0].classList.add('in')
-}
-
-function runAnimation() {
-    NUMS.forEach((num,idx) => {
-        let nextToLast = NUMS.length - 1
-        num.addEventListener('animationend', (e) => {
-            if(e.animationName === 'goIn' && idx !== nextToLast) {
-                num.classList.remove('in')
-                num.classList.add('out')
-            } else if (e.animationName === 'goOut' && num.nextElementSibling) {
-                num.nextElementSibling.classList.add('in')
-            } else {
-                COUNTER.classList.add('hide')}})})
 }
 
 function replay(){
@@ -207,15 +200,49 @@ function instructionStart(){
     PRESTART.classList.add('visible')
 }
 
+/* shotUpdate() updates the shot tally in the game and runAnimation() allows
+for the score to be animated using the CSS transformations and classes smoothly
+once the game has begun.
+*/
+function shotUpdate(){
+    shotTally++
+    SHOT_TALLY_CURRENT.innerText = shotTally
+}
+
+function runAnimation() {
+    NUMS.forEach((num,idx) => {
+        let nextToLast = NUMS.length - 1
+        num.addEventListener('animationend', (e) => {
+            if(e.animationName === 'goIn' && idx !== nextToLast) {
+                num.classList.remove('in')
+                num.classList.add('out')
+            } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+                num.nextElementSibling.classList.add('in')
+            } else {
+                COUNTER.classList.add('hide')}})})
+}
+
+/* The functions below relate to the scoring of the game one starts the score (startScore)
+and also countdown timer.
+stopScore() obviously stops the game by revealing the results.
+allScores() visually displays the scores achieved in the game.
+*/
 function startScore (){
     PRESTART.classList.add('hiddenscore')
     PRESTART.classList.remove('visible')
     setTimeout(stopScore, 20000)
 }
 
-function shotUpdate(){
-    shotTally++
-    SHOT_TALLY_CURRENT.innerText = shotTally
+function stopScore () {
+    FINAL_SCORE.innerText = `${parseInt(FINAL_SCORE.innerText)} Top Score!`
+    SCORE_SCREEN.classList.remove('hiddenscore')
+    SCORE_SCREEN.classList.add('visible')
+    PERFECT_SCORE.classList.add('hiddenscore')
+    PERFECT.classList.add('hiddenscore')
+    GAME_SCORE.innerText = FINAL_SCORE.innerText
+    FINAL_SCORE.style.fontSize = '35px'
+    NUMBERS.classList.remove('out')
+    allScores()
 }
 
 function allScores (){ 
@@ -226,3 +253,5 @@ function allScores (){
     TARGET_TALLY_ID.innerHTML = `${targetTally} Targets Hit`
     PERFECT_TALLY_ID.innerHTML = `${perfectPercentage}% Perfect Shots`
 }
+
+module.exports = { incrementScore }
--- functions.test.js
+++ functions.test.js
@@ -1,3 +1,11 @@
+beforeAll(()=>{
+    let fs = require('fs');
+    let fileContents = fs.readFileSync('index.html','utf-8');
+    document.open();
+    document.write(fileContents);
+    document.close()
+})
+
 const functions = require('./functions')
 
 test('Adds 2 + 2 to equal 4', () => {
